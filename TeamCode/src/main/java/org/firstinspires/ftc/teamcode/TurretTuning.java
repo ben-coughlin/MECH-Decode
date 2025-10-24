8d472846a3653e824b4ea59ad84fae13826032e3
Ryan Hall <ryan.hall@mullenmail.com>
2025-10-24 16:11:00 -0600

    New Boolean could (potentially) fix wire eating

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTuning.java
@@ -5,38 +5,39 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Turret Tuning")
 public class TurretTuning extends RobotMasterPinpoint
 {
+    public boolean hasBeenActivated = false;
     @Override
-    public void init() {
-        super.init();
-    }
+    public void init() {}
     @Override
-    public void init_loop() {
-        super.init_loop();
-    }
+    public void init_loop() {}
     @Override
-    public void start() {
-        super.start();
-    }
+    public void start() {}
     @Override
     public void mainLoop() {
         super.mainLoop();
-        if (gamepad1.right_bumper) {
+        if (gamepad1.right_bumper && !hasBeenActivated) {
             turret.setTurretPower(turret.getTurretPower() + 1);
+            hasBeenActivated = true;
         }
-        else if (gamepad1.left_bumper) {
+        else if (gamepad1.left_bumper && !hasBeenActivated) {
             turret.setTurretPower(turret.getTurretPower() - 1);
+            hasBeenActivated = true;
         }
-        if (gamepad1.dpad_up) {
+        if (gamepad1.dpad_up && !hasBeenActivated) {
             turret.setFlywheelPower(turret.getFlywheelRPM() + 0.01);
+            hasBeenActivated = true;
         }
-        else if (gamepad1.dpad_down) {
+        else if (gamepad1.dpad_down && !hasBeenActivated) {
             turret.setFlywheelPower(turret.getFlywheelRPM() - 0.01);
+            hasBeenActivated = true;
         }
-        if (gamepad1.dpad_right) {
+        if (gamepad1.dpad_right && !hasBeenActivated) {
             turret.setHoodPos(turret.getHoodPos() + 0.05);
+            hasBeenActivated = true;
         }
-        else if (gamepad1.dpad_left) {
+        else if (gamepad1.dpad_left && !hasBeenActivated) {
             turret.setHoodPos(turret.getHoodPos() - 0.05);
+            hasBeenActivated = true;
         }
         telemetry.addData("Current Turret Position", turret.getTurretPos());
         telemetry.addData("Current Flywheel RPM", turret.getFlywheelRPM());
@@ -47,7 +48,5 @@ public class TurretTuning extends RobotMasterPinpoint
         telemetry.update();
     }
     @Override
-    public void stop(){
-        super.stop();
-    }
+    public void stop(){}
 }
